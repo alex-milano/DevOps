feec529a45a51fe42a0bcc3b2f130d47afd16967
Alex Milano <alexjmilano@gmail.com>
2023-04-26 13:44:57 -0300

    Create webdeploy_v1.sh

---
+++ webdeploy_v1.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+user=$(id -u)
+echo $user
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo "el usuario debe ser root"
+    exit
+fi
+
+##update
+apt update
+apt install -y git curl apache2
+
+sleep1
+##iniciar el servicio de apache
+echo "Iniciando servicio de apache"
+systemctl start apache2
+systemctl enable apache2
+
+sleep1
+#web
+git clone devops-memory https://github.com/roxsross/devops-static-web.git
+cp -r devops-static-web/* /var/www/html/
+ls -ltr /var/www/html/
+echo "pagina instalada"
+
+sleep1
+##test 
+echo "testeando web"
+curl localhost
